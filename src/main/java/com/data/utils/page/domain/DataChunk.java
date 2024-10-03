eb3aa8334e6b8ca85ff8cdaa4b03f77ab772571e
Vishwas Saini <vishwas.saini@fincity.com>
2024-10-03 23:54:47 +0530

    Implemented DataChunk.java

--- src/main/java/com/data/utils/page/domain/DataChunk.java
+++ src/main/java/com/data/utils/page/domain/DataChunk.java
@@ -1,82 +1,94 @@
 package com.data.utils.page.domain;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 
-public class DataChunk<T> implements Slice<T>, Serializable {
+public abstract class DataChunk<T> implements Slice<T>, Serializable {
+
+  @Serial private static final long serialVersionUID = 5262034506603260731L;
+
+  private final List<T> content = new ArrayList<>();
+  private final Pageable pageable;
+
+  protected DataChunk(List<T> content, Pageable pageable) {
+    this.content.addAll(content);
+    this.pageable = pageable;
+  }
 
   @Override
   public int getNumber() {
-    return 0;
+    return pageable.isPaged() ? pageable.getPageNumber() : 0;
   }
 
   @Override
   public int getSize() {
-    return 0;
+    return pageable.isPaged() ? pageable.getPageSize() : content.size();
   }
 
   @Override
   public int getNumberOfElements() {
-    return 0;
+    return content.size();
   }
 
   @Override
   public List<T> getContent() {
-    return List.of();
+    return content;
   }
 
   @Override
   public boolean hasContent() {
-    return false;
+    return !content.isEmpty();
   }
 
   @Override
   public Sort getSort() {
-    return null;
+    return pageable.getSort();
   }
 
   @Override
   public boolean isFirst() {
-    return false;
+    return getNumber() == 0;
   }
 
   @Override
   public boolean isLast() {
-    return false;
+    return !hasNext();
   }
 
   @Override
   public boolean hasNext() {
-    return false;
+    return getNumberOfElements() == getSize();
   }
 
   @Override
   public boolean hasPrevious() {
-    return false;
+    return getNumber() > 0;
   }
 
   @Override
   public Pageable nextPageable() {
-    return null;
+    return hasNext() ? pageable.next() : Pageable.unpaged();
   }
 
   @Override
   public Pageable previousPageable() {
-    return null;
-  }
-
-  @Override
-  public <U> Slice<U> map(Function<? super T, ? extends U> converter) {
-    return null;
+    return hasPrevious() ? pageable.previousOrFirst() : Pageable.unpaged();
   }
 
   @Override
   public Iterator<T> iterator() {
-    return null;
+    return content.iterator();
+  }
+
+  public <U> List<U> getConvertedContent(Function<? super T, ? extends U> converter) {
+    return this.stream().map(converter).collect(Collectors.toList());
   }
 }
